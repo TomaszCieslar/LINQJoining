796c31a19ad93f765fb507aab3f11a0613b3d980
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-27 06:09:28 +0200

    final solution

---
+++ LINQJoining/MAnufacturer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQJoining
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year { get; set; }
+
+    }
+}
--- LINQJoining/Program.cs
+++ LINQJoining/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace LINQJoining
 {
@@ -6,7 +9,57 @@ namespace LINQJoining
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcessFile("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
+
+            var query = cars.Join(manufacturers,
+                                    c => c.Manufacturer,
+                                    m => m.Name, (c, m) => new
+                                    {
+                                        m.Headquarters,
+                                        c.Name,
+                                        c.Combined
+                                    })
+                                .OrderByDescending(c => c.Combined)
+                                .ThenBy(c => c.Name);
+
+
+
+
+            foreach (var item in query.Take(10))
+            {
+                Console.WriteLine($"{item.Headquarters} {item.Name} {item.Combined}");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return
+                 File.ReadAllLines(path)
+                  .Skip(1)
+                  .Where(line => line.Length > 1)
+                  .Select(Car.ParseFromCSV).ToList();
+
+        }
+
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query = 
+                File.ReadAllLines(path)
+                    .Where(l => l.Length > 1)
+                    .Select(l =>
+                    {
+                        var columns = l.Split(',');
+                        return new Manufacturer
+                        {
+                            Name = columns[0],
+                            Headquarters = columns[1],
+                            Year = int.Parse(columns[2])
+                        };
+                    });
+            return query.ToList();
         }
     }
 }
